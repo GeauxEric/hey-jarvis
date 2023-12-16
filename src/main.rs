170c2c4d5409e603936c3cb4ecef0e27419f4314
Yun Ding <yunding.eric@gmail.com>
2023-12-15 20:50:36 -0800

    rename package name

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 use std::env;
+use std::time::Duration;
 
 use anyhow::Result;
 use chatgpt::config::ModelConfigurationBuilder;
 use chatgpt::prelude::{ChatGPT, ChatGPTEngine};
-use clap::{arg, ArgAction, command, Command, value_parser};
+use clap::{arg, command, value_parser, ArgAction, Command};
 
 fn get_command() -> Command {
-    command!()
-        .arg_required_else_help(true)
-        .subcommand(
-            Command::new("ask")
-                .arg_required_else_help(true)
-                .about("Ask a single question").arg(
+    command!().arg_required_else_help(true).subcommand(
+        Command::new("ask")
+            .arg_required_else_help(true)
+            .about("Ask a single question")
+            .arg(
                 arg!([question])
                     .help("The question to ask")
                     .required(true)
                     .action(ArgAction::Set)
                     .value_parser(value_parser!(String)),
             ),
-        )
+    )
 }
 
 #[tokio::main]
@@ -30,11 +30,12 @@ async fn main() -> Result<()> {
         api_key,
         ModelConfigurationBuilder::default()
             .engine(ChatGPTEngine::Gpt4)
+            .timeout(Duration::from_secs(60))
             .build()?,
     )?;
     println!("Using {}", gpt_client.config.engine);
 
-    if let Some(question) = matches.subcommand_matches("question") {
+    if let Some(question) = matches.subcommand_matches("ask") {
         let q = question.get_one::<String>("question").unwrap().to_string();
         let resp = gpt_client.send_message(q).await?;
         println!("Response: {}", resp.message().content)
